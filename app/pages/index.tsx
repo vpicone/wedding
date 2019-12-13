be62ea5a9ec523385257e6f2bc53ed0194e39755
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-12-12 22:12:19 -0600

    init

---
+++ app/pages/index.tsx
@@ -0,0 +1,30 @@
+import { request } from "graphql-request";
+import useSWR from "swr";
+
+function Profile() {
+  const uri = `/admin/api`;
+
+  const { data, error } = useSWR(
+    `{
+      allTodos {
+        name
+      }
+    }`,
+    query => request(uri, query)
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  return (
+    <div>
+      todos:{" "}
+      <ul>
+        {data.allTodos.map((todo: any) => (
+          <li>{todo.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Profile;
---
+++ index.js
@@ -0,0 +1,61 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { Text, Password, Checkbox, Select } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { NextApp } = require('@keystonejs/app-next');
+const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
+const { byTracking } = require('@keystonejs/list-plugins');
+
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+
+const { distDir } = require('./config');
+
+const PROJECT_NAME = 'keystone';
+
+const keystone = new Keystone({
+  name: PROJECT_NAME,
+  adapter: new Adapter({
+    mongoUri: process.env.MONGO_URI || '',
+  }),
+});
+
+keystone.createList('Todo', {
+  schemaDoc: 'A list of things which need to be done',
+  fields: {
+    name: { type: Text, schemaDoc: 'This is the thing you need to do' },
+  },
+});
+
+keystone.createList('User', {
+  fields: {
+    name: { type: Text, isRequired: true },
+    email: {
+      type: Text,
+    },
+    isAdmin: { type: Checkbox },
+    plusOne: { type: Checkbox },
+    password: { type: Password },
+    status: {
+      type: Select,
+      options: 'Pending, Declined, Accepted',
+      defaultValue: 'Pending',
+    },
+  },
+  labelResolver: item => `${item.name + (item.plusOne ? ' +1' : '')}`,
+  plugins: [byTracking()],
+});
+
+const authStrategy = keystone.createAuthStrategy({
+  type: PasswordAuthStrategy,
+  list: 'User',
+  config: {
+    identityField: 'username', // default: 'email'
+    secretField: 'password', // default: 'password'
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [new GraphQLApp(), new AdminUIApp({}), new NextApp({ dir: 'app' })],
+  distDir,
+};
